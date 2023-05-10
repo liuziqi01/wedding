53d136ea42599b0ef57ec2bf25bb69325b38c6ac
Ziqi Liu <liuziqi01@gmail.com>
2023-05-10 01:55:53 -0700

    Initiate Node.js, add page specified css. render floral and golden frame

---
+++ public/js/generate_table_content.js
@@ -0,0 +1,41 @@
+// Load the data from the JSON file
+
+var alltable=[];
+
+const xhr = window.XMLHttpRequest ? new XMLHttpRequest(): new ActiveXObject("Microsoft.XMLHTTP"); 
+xhr.open('GET', '../resources/names.txt', false);
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const text = xhr.responseText;
+    const lines = text.split('\n');
+    lines.forEach(line => {
+      // Process each line here
+      const names = line.split(',');
+      alltable.push(names);
+      console.log(names);
+    });
+  }
+};
+xhr.send();
+
+		var tableNumber = 1;
+				// Create a table for each table in the data
+		alltable.forEach(tableData => {
+			var table = document.createElement('table');
+			tableData.sort();
+			table.id = "table" + tableNumber;
+			table.innerHTML = `
+			<thead>
+			<tr>
+			<th colspan="3">Table ${tableNumber}</th>
+			</tr>
+			</thead>
+			<tbody>
+			${tableData.map(name => `<tr><td>${name}</td></tr>`).join('')}
+			</tbody>
+			`;
+			document.querySelector('.tables').appendChild(table);
+			tableNumber++;
+		});
+
+
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('html', require('ejs').renderFile);
+//app.set('views', path.join(__dirname, 'views'));
+//_app.set('view engine', 'html');
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Define a route to serve the HTML file
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/seat/floral', function(req, res) {
+  var cssFile = "floral.css"
+  res.render( path.join(__dirname, 'public/views/') +'seat.html', { cssFile });
+});
+
+app.get('/seat/golden', function(req, res) {
+  var cssFile = "golden.css"
+  res.render( path.join(__dirname, 'public/views/') +'seat.html', { cssFile });
+});
+
+
+// Start the server
+const port = 3000;
+app.listen(port, function() {
+  console.log('Server is running at http://localhost:' + port);
+});
